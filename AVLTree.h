8ef8f7f29d33682036882ab908ff7666df884e2f
Zohar <a@b>
2024-02-17 20:44:39 +0200

    did some shite in AVLTree.

--- AVLTree.h
+++ AVLTree.h
@@ -41,46 +41,81 @@ public:
 
 
 private:
+	//fields
 	int n; //number of nodes
 	node* root;
 	COMP comp;
 
-	height(node* head) const{
-		head == nullptr ? return -1: return head->height;
-	}
+	//recursive utility functions
+	node* insertUtil(node* head, T key);
+	node* removeUtil(node* head, T key);
+	node* searchUtil(node* head, T key) const;
+	void destroy(node* head);
 
-	void destroy(node* head) {
+	//general utility functions
+	int height(node* head) const {
+		 return (head == nullptr) ? -1 : head->height;
+	}
+	int balanceFactor(node* head) const{
+		return (head == nullptr) ? 0 : height(head->left) - height(head->right);
+	}
+	node* balanceTree(node* head, int balanceFactor);
+
+    // Roll Functions
+    Node *rollLeftLeft(Node *node);
+    Node *rollLeftRight(Node *node);
+    Node *rollRightLeft(Node *node);
+    Node *rollRightRight(Node *node);
+
+};
+//--------------recursive utility functions--------------//
+template<typename T, typename COMP>
+typename AVLTree<T, COMP>::node* AVLTree<T, COMP>::insertUtil(AVLTree<T, COMP>::node* head, T key) {
+	if (head == nullptr) {
+		node newNode = new node(key);
+		n++; //aloc error wont change n
+		return newNode;
+	}
+	if (comp(key, head->key) = -1) {
+		head->left = insertUtil(head->left, key);
+	}
+	else if (comp(key, head->key) = 1) {
+		head->right = insertUtil(head->right, key);
+	}
+	head->height = 1 + max(height(head->left), height(head->right));
+	return balanceTree(head, balanceFactor(head));
+}
+
+
+template<typename T, typename COMP>
+	void AVLTree<T, COMP>::destroy(AVLTree<T, COMP>::node* head) {
 		if (head == nullptr) return;
 		destroy(head->left);
 		destroy(head->right);
 		delete head;
 	}
-	node* insertUtil(node* head, T key) {
-		if (head == nullptr) {
-			node newNode = new node(key);
-			n++; //aloc error wont change n
-			return newNode;
-		}
-		if (comp(key, head->key) = -1) {
-			head->left = insertUtil(head->left, key);
-		}
-		else if (comp(key, head->key) = 1) {
-			head->right = insertUtil(head->right, key);
-		}
-		head->height = 1 + max(height(head->left), height(head->right));
-		int balance = height(head->left) - height(head->right);
-		if (balance == 2) {
-			//fix imbalance, return new head
-		}
-		else if (balance == -2) {
-			//fix imbalance, return new head
-		}
-	}
-	node* removeUtil(node* head, T key) {
-		if (head == nullptr) return nullptr;
-		if (comp(key, head->key) == 0){
 
+//--------------general utility functions--------------//
+template<typename T, typename COMP>
+typename AVLTree<T, COMP>::node* AVLTree<T, COMP>::balanceTree(AVLTree<T, COMP>::node* head, int balanceFactor) {
+	if (balanceFactor > 1) {
+		if(balanceFactor(head->left) > -1) {
+			return rollLeftLeft(head);
+		}
+		else {
+			return rollLeftRight(head);
 		}
 	}
-};
+	else if (balanceFactor < -1) {
+		if(balanceFactor(head->right) > -1) {
+			return rollRightLeft(head);
+		}
+		else {
+			return rollRightRight(head);
+		}
+	}
+	return head;
+}
+
+//--------------Roll Functions--------------//
 #endif
